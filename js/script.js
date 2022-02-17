a6d88b56dca97dc466d1757acbfbe7468d9c90b0
Tamanna-akter <atamanna577@gmail.com>
2022-02-17 16:33:45 +0600

    secondcommit

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 document.getElementById('calculate-button').addEventListener('click', function () {
     const incomeText = document.getElementById('income-input');
     const incomeAmount = (incomeText.value);
-    incomeText.value = "";
+    // incomeText.value = "";
 
     const foodText = document.getElementById('food-input');
     const foodAmount = foodText.value;
@@ -21,6 +21,21 @@ document.getElementById('calculate-button').addEventListener('click', function (
 });
 
 document.getElementById('save-button').addEventListener('click', function () {
-    const savingsText = document.getElementById('savings-balance');
+    const savingsInputText = document.getElementById('save-input');
+    const incomeText = document.getElementById('income-input');
+    const incomeAmount = incomeText.value;
+    const savingsPercentValue = savingsInputText.value;
+    const savings = (incomeAmount * parseInt(savingsPercentValue) / 100);
+    const savingsBalance = document.getElementById('savings-balance');
+    savingsBalance.innerText = savings;
+    const currentTotalBalance = document.getElementById('total-balance').innerText;
+    const remainingBalanceText = document.getElementById('remaining-balance');
+    const remainingBalance = remainingBalanceText.innerText;
+
+    const remaining = currentTotalBalance - savingsBalance.innerText;
+    remainingBalanceText.innerText = remaining;
+
+
+
 
 })

e2b4766f79292f29ff96678371283ec6ebeb2953
Tamanna-akter <atamanna577@gmail.com>
2022-02-17 23:06:45 +0600

    last-commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Handle button event
 
 document.getElementById('button-1').addEventListener('click', function () {
     calculateTotal();
@@ -7,21 +8,27 @@ document.getElementById('button-1').addEventListener('click', function () {
 document.getElementById('button-2').addEventListener('click', function () {
     savingsTotal();
 })
+// declare input field function
 
 function inputField(inputFieldId) {
     const inputArea = document.getElementById(inputFieldId).value;
     const inputValue = parseFloat(inputArea);
     return inputValue;
 }
+
+// declare budget calculation function
+
 function calculateTotal() {
 
     const incomeAmount = inputField('income-input');
     const foodAmount = inputField('food-input');
     const rentAmount = inputField('rent-input');
     const clothesAmount = inputField('clothes-input');
-
+    // calculate expense amount
     const expenseAmount = foodAmount + rentAmount + clothesAmount;
     const balanceTotal = incomeAmount - expenseAmount;
+    // error handle
+
     if (isNaN(incomeAmount) || incomeAmount < 0 || incomeAmount == "") {
         return alert('Please input valid amount of money in number format');
     }
@@ -43,7 +50,7 @@ function calculateTotal() {
     }
 
 }
-
+// savings function
 function savingsTotal() {
 
     const balanceText = document.getElementById('total-balance').innerText;
@@ -55,7 +62,7 @@ function savingsTotal() {
     const incomeText = document.getElementById('income-input').value;
     const incomeAmount = parseFloat(incomeText);
 
-
+    // find percentage value 
     const savings = (incomeAmount * savingsAmount) / 100;
     const remainingBalance = balanceAmount - savings;
 
